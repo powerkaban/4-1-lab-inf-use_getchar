94c56dafff7c77b405b00759c6b9c928e1124f5d
powerkaban <alesh.belov2016@yandex.ru>
2024-06-12 12:53:47 +0300

    it s all ready

--- 4(1)ая лаба по инфе getchar/4(1)ая лаба по инфе getchar.cpp
+++ 4(1)ая лаба по инфе getchar/4(1)ая лаба по инфе getchar.cpp
@@ -7,35 +7,32 @@ int main()
 
         char MAX_STR;
         setlocale(LC_ALL, "rus");
-        FILE* file, * file2;
-        char c, filename[999];
+        FILE *file, *file2;
+        char  filename[10]{0};
+        char c;
         int i = 0;
         int second = 0;
         fopen_s(&file, "myfile.txt", "r");
         printf("Ищем сколько двоеточий в файле ");
-        while ((c = fgetc(file)) != EOF) {
-            
 
-            if (c == ':') {
-                while (((c = fgetc(file)) != EOF )&&( c != ':')){
-
-                    filename[i] = c;
-                    i++;
-                };
-            
+        if (file){
+            while ((c = fgetc(file)) != EOF) {
+                if (c == ':') {
+                    while (((c = fgetc(file)) != EOF) && (c != ':')) {
+                        filename[i] = c;
+                        i++;
+                    };
+                }
             }
-            
-
-
         }
 
         printf("разделение на две части");
         fclose(file);
+
         fopen_s(&file2, "input.txt", "w");
-        for (int j = 0; j < i;j++) {
-            fputs(filename, file2);
-        }
+
+        fputs(filename, file2);
 
         fclose(file2);
-    
+     
 }
